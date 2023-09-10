e2a38a9c5479b0aec9fe508a42482f995f5cecf3
Alex <hbostic@gmail.com>
2023-09-09 21:36:59 -0400

    Resolved path issues for code from two seasons ago, and update it to last season folder restructuring

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/CommandTeleOp.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.opmodes.teleop;
+
+import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.mechanisms.drivetrain.CreateDriveTrainMechanism;
+
+
+
+
+
+@TeleOp(name="Command Combined TeleOp")
+public class CommandTeleOp extends CommandOpMode {
+
+    @Override
+    public void initialize() {
+
+        GamepadEx driver1 = new GamepadEx(gamepad1);
+        GamepadEx driver2 = new GamepadEx(gamepad2);
+
+        CreateDriveTrainMechanism createDriveTrain = new CreateDriveTrainMechanism(hardwareMap, "drive", driver1, telemetry, true);
+
+    }
+    public void execute(){
+
+    }
+}
