4a1319f3c5e319a44b58d9add5cc1c965b2a0aed
Alex <hbostic@gmail.com>
2024-01-04 14:12:09 -0500

    Added Meep Meep

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/paths/RedAllianceNonStageSidePath1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/paths/RedAllianceNonStageSidePath1.java
@@ -148,7 +148,7 @@ public class RedAllianceNonStageSidePath1 {
                 /*.addDisplacementMarker(() -> {
                     turnCommand.schedule();
                 })*/
-                .lineToLinearHeading(new Pose2d(-36,-50, Math.toRadians(220)))
+                .lineToLinearHeading(new Pose2d(-36,-50, Math.toRadians(90)))
                 .build();
 
         Trajectory traj2 = drive.trajectoryBuilder(traj1.end())
