dc5ab0b6cc9bf06004637c46acdf655dec234f98
DESKTOP-AUURQ7F\BC4HSTEM <izeyahthagreat@gmail.com>
2024-01-04 18:36:34 -0500

    Change the velocity of the robot to 35 and tested it's movment.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/drivetrain/constants/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/drivetrain/constants/DriveConstants.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode.mechanisms.drivetrain.constants;
 
-import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
@@ -15,7 +14,7 @@ import com.qualcomm.robotcore.hardware.PIDFCoefficients;
  * These are not the only parameters; some are located in the localizer classes, drive base classes,
  * and op modes themselves.
  */
-@Config
+
 public class DriveConstants {
 
     /*
