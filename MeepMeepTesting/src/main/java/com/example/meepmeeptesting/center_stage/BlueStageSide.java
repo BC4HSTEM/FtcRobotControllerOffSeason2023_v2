da6cdf59425c2c0755a52cc672adad7d8e3c5064
DESKTOP-AUURQ7F\BC4HSTEM <izeyahthagreat@gmail.com>
2024-01-10 20:48:37 -0500

    Updated Blue side stage start with comments. Updated the paths with the robot heading turning the other direction before it pulls back and parks

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/center_stage/BlueStageSide.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/center_stage/BlueStageSide.java
@@ -17,23 +17,18 @@ public class BlueStageSide {
                 .setConstraints(37.105101137333385, 30, 16.897379840537297, Math.toRadians(180), 13.35)
                 // Option: Set theme. Default = ColorSchemeRedDark()
                 .setColorScheme(new ColorSchemeRedDark())
-                // place purple pixel and park 
+                // place purple pixel and park
                 .followTrajectorySequence(drive ->
                         drive.trajectorySequenceBuilder(new Pose2d(12, 60, Math.toRadians(270)))
                                 .lineToLinearHeading(new Pose2d(12,37, Math.toRadians(270)))
-                                .lineToLinearHeading(new Pose2d(12, 60, Math.toRadians(0)))
-                                .lineToLinearHeading(new Pose2d(60, 60, Math.toRadians(0)))
-                                //.turn(Math.toRadians(90))
-                                //.forward(30)
+                                // Drop Purple Pixel on the spike mark here
                                 .addDisplacementMarker(() -> {
-                                    /* Everything in the marker callback should be commented out */
-
-                                    // bot.shooter.shoot()
-                                    // bot.wobbleArm.lower()
+                                    // Drop Pixel based on object
+                                    // Collapse the grabber wrist to arm
                                 })
-                                /*.turn(Math.toRadians(90))
-                                .splineTo(new Vector2d(10, 15), 0)
-                                .turn(Math.toRadians(90))*/
+                                .lineToLinearHeading(new Pose2d(12, 60, Math.toRadians(0)))
+                                // Park in the backstage
+                                .lineToLinearHeading(new Pose2d(60, 60, Math.toRadians(0)))
                                 .build()
                 );
 
