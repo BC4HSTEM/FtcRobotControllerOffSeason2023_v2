810b11012828367a3f38cafd4faf449fb0812e0a
DESKTOP-AUURQ7F\BC4HSTEM <izeyahthagreat@gmail.com>
2024-01-17 13:13:49 -0500

    See previous commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/UtilityCameraFrameCapture.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/UtilityCameraFrameCapture.java
@@ -31,12 +31,14 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.robotcontroller.external.samples;
+package org.firstinspires.ftc.teamcode.opmodes.teleop;
 
 import android.util.Size;
+
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.vision.VisionPortal;
@@ -56,18 +58,17 @@ import java.util.Locale;
  *
  * In OnBot Java, use "Add File" to add this OpMode from the list of Samples.
  */
-
-@TeleOp(name = "Utility: Camera Frame Capture", group = "Utility")
 @Disabled
+@TeleOp(name = "Utility: Camera Frame Capture", group = "Utility")
 public class UtilityCameraFrameCapture extends LinearOpMode
 {
     /*
      * EDIT THESE PARAMETERS AS NEEDED
      */
-    final boolean USING_WEBCAM = false;
+    final boolean USING_WEBCAM = true;
     final BuiltinCameraDirection INTERNAL_CAM_DIR = BuiltinCameraDirection.BACK;
-    final int RESOLUTION_WIDTH = 640;
-    final int RESOLUTION_HEIGHT = 480;
+    final int RESOLUTION_WIDTH = 320;
+    final int RESOLUTION_HEIGHT = 240;
 
     // Internal state
     boolean lastX;
